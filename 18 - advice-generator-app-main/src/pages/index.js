50a54191ef6cb290679dff984166597a47e00881
Samuel Seve <samuelseve1@gmail.com>
2022-04-19 10:20:27 -0300

    att

--- 18 - advice-generator-app-main/src/pages/index.js
+++ 18 - advice-generator-app-main/src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta http-Equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Desafio 18</title>
