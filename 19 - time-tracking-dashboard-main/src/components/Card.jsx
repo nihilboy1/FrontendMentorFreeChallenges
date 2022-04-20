ce20dbd7aef5b162ce02eb2655c0ee794241c56e
Samuel Seve <samuelseve1@gmail.com>
2022-04-19 21:16:48 -0300

    ajustes

--- 19 - time-tracking-dashboard-main/src/components/Card.jsx
+++ 19 - time-tracking-dashboard-main/src/components/Card.jsx
@@ -6,13 +6,15 @@ import data from '../DATA/data.json'
 function Card({ id, chave, title, image, time }) {
   return (
     <ColorDiv id={id} className="container_colors">
-      <img className="background_icon" src={image} alt="" />
+      <div className='image_box'>
+        <img className="background_icon" src={image} alt="" />
+      </div>
       <div className={'container_data'}>
-        <div id="div_left">
+        <div className="div_left">
           <h1>{title}</h1>
           <p>{data[chave].timeframes[time].current}hrs</p>
         </div>
-        <div id="div_right">
+        <div className="div_right">
           <img src={ellipsis} alt="" />
           <p>
             Last{' '}
