0883f0f671f0bdb25231af32872b81b6eab98286
Samuel Seve <samuelseve1@gmail.com>
2022-06-18 15:13:36 -0300

    fonts

--- 27 - ip-address-tracker-master/project/src/pages/_document.tsx
+++ 27 - ip-address-tracker-master/project/src/pages/_document.tsx
@@ -15,13 +15,12 @@ export default class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
             href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
