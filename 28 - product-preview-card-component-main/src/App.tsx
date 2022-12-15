67c909cd26a82d4c38be520d7645d0b7864e47b7
nihilboy1 <samuelseve1@gmail.com>
2022-12-15 11:33:31 -0300

    projeto 28

---
+++ 28 - product-preview-card-component-main/src/App.tsx
@@ -0,0 +1,35 @@
+import { useMediaQuery } from '@chakra-ui/react'
+import { ShoppingCart } from 'phosphor-react'
+import desktopImage from '../public/image-product-desktop.jpg'
+import mobileImage from '../public/image-product-mobile.jpg'
+import S from './styles/App.module.scss'
+
+function App() {
+  const [isShorterThan550] = useMediaQuery('(max-width: 550px)')
+
+  return (
+    <main className={S.main_box}>
+      <img src={isShorterThan550 ? mobileImage : desktopImage} alt="" />
+      <div className={S.text_box}>
+        <header>
+          <span className={S.perfume}>PERFUME</span>
+          <h1 className={S.title}>Gabrielle Essence Eau De Parfum</h1>
+        </header>
+        <p className={S.main_text}>
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL
+        </p>
+        <div className={S.prices_box}>
+          <p className={S.main_price}>$149.99</p>
+          <p className={S.other_price}>$169.99</p>
+        </div>
+        <button>
+          <ShoppingCart weight="bold" size={18} color="white" />
+          <span>Add to Cart</span>
+        </button>
+      </div>
+    </main>
+  )
+}
+
+export default App
