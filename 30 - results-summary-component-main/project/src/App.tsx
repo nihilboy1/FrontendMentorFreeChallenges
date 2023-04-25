0cdcc502be526792c075845bd323e6e653352220
nihilboy1 <samuelseve1@gmail.com>
2023-04-25 09:58:01 -0300

    att

---
+++ 30 - results-summary-component-main/project/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <main className="mainContainer">
+      <section>
+        <h1>Your Result</h1>
+        <div>
+          <h2>76</h2>
+          <span>of 100</span>
+        </div>
+        <p>Great</p>
+        <p>
+          You scored higher than 65% of the people who have taken these tests
+        </p>
+      </section>
+      <section></section>
+    </main>
+  );
+}
+
+export default App;
